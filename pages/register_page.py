99a62c7a74c70ffa444b07c2f19ac1cf7c553f22
Sadikshya Khatri Chhetri <sadikshyakhatrichhetri@gmail.com>
2026-06-16 19:57:11 +0545

    Remove pycache

--- pages/register_page.py
+++
@@ -1,5 +0,0 @@
-class RegisterPage:
-    def __init__(self, page):
-        self.page = page
-    def open(self):
-        self.page.goto("https://demo.automationtesting.in/Register.html")
--- tests/test_register.py
+++
@@ -1,5 +0,0 @@
-from pages.register_page import RegisterPage
-def test_registerpageload(page):
-    register = RegisterPage(page)
-    register.open()
-    assert "Register" in page.title()
